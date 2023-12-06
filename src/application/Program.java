631ca4a1b234863074daccf6e02979e0d64c0c0f
edsolima <ewerton.silva.o.lima@gmail.com>
2023-12-06 20:45:14 -0300

    chess layer and print the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,18 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		ChessMatch chessMatch = new ChessMatch();
 		
+		UI.printBoard(chessMatch.getPieces());
 
-		Position Pos = new Position(3, 5);
+
+
 		
-		
-		System.out.println(Pos);
 	}
 
 }
